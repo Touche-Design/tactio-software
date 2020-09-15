e7bc70076f13adaa31c4bf3597b743392305cadd
varunm99 <vmadabushi3@gatech.edu>
2020-09-15 14:45:44 -0400

    Commented code and added start character detection

--- visualization/SingleGrid.py
+++ visualization/SingleGrid.py
@@ -7,13 +7,14 @@ import os
 import serial
 import numpy as np
 
+'''
+The GridPoint Widget represents 1 colored square in a sensor's grid
+'''
 
 class GridPoint(QtWidgets.QWidget):
-
     def __init__(self, *args, **kwargs):
         super(QtWidgets.QWidget, self).__init__(*args, **kwargs)
         self.setAutoFillBackground(True)
-        #self.palette.setColor(QtGui.QPalette.Window, Qt.red)
         p = self.palette()
         p.setColor(self.backgroundRole(), QtGui.QColor(random.randrange(255), random.randrange(255), random.randrange(255)))
         self.setPalette(p)
@@ -25,65 +26,78 @@ class GridPoint(QtWidgets.QWidget):
         p.setColor(self.backgroundRole(), color)
         self.setPalette(p)
 
+'''
+This widget is the main window of the visualization. It has a grid of 4 x 4 grid points which change color based on input
+'''
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self,  *args, **kwargs):
         super(QtWidgets.QMainWindow, self).__init__(*args, **kwargs)
-        self.setWindowTitle("Tactio 1")
-        self.gridWidgets = [[GridPoint(), GridPoint(), GridPoint(), GridPoint()], 
+        self.setWindowTitle("Tactio")
+        # Grid Widgets stored in a member variable array
+        self.gridWidgets = [[GridPoint(), GridPoint(), GridPoint(), GridPoint()],
         [GridPoint(), GridPoint(), GridPoint(), GridPoint()],
         [GridPoint(), GridPoint(), GridPoint(), GridPoint()],
         [GridPoint(), GridPoint(), GridPoint(), GridPoint()]]
 
+        # We arrange rows using HBoxLayouts
         hboxes = [QtWidgets.QHBoxLayout(), QtWidgets.QHBoxLayout(), QtWidgets.QHBoxLayout(), QtWidgets.QHBoxLayout()]
+
+        # And a single VBoxLayout arranges all the rows into a grid
         vbox = QtWidgets.QVBoxLayout()
+
+        # Loop through all widgets and add them accordingly
         for i in range(len(hboxes)):
             for j in range(len(self.gridWidgets[i])):
                 hboxes[i].addWidget(self.gridWidgets[i][j])
-                hboxes[i].addSpacing(10)
-            vbox.addItem(hboxes[i])
+                hboxes[i].addSpacing(10) #Spacing between columns
+            vbox.addItem(hboxes[i]) #Add each row to the VBoxLayout
         mainWidget = QtWidgets.QWidget()
         mainWidget.setLayout(vbox) 
         self.setCentralWidget(mainWidget)
 
+        # QTimer kicks off every 0.2 seconds to update the visualization
         self.vizTimer = QtCore.QTimer()
-        self.vizTimer.setInterval(int(1000/5))
+        self.vizTimer.setInterval(int(1000/5)) # Convert Hz to ms interval
         self.vizTimer.timeout.connect(self.timerCallback)
         self.vizTimer.start()
 
-        self.input_ser = serial.Serial('/dev/ttyACM0') #Serial port for Arduino Uno
+        #TODO: Implement Communication Rate Limiting
+
+        self.input_ser = serial.Serial('/dev/ttyACM0') #Serial port for STM32
         self.input_ser.baudrate = 9600
 
-    def timerCallback(self):
-        array = self.parseSerial()
-        for i in range(len(self.gridWidgets[0])):
+    def timerCallback(self): # Kicks off when QTimer has a timeout event
+        array = self.parseSerial() # Turns serial data into 2D array of integer values
+        for i in range(len(self.gridWidgets[0])): 
             for j in range(len(self.gridWidgets)):
-                self.gridWidgets[i][j].setColor(QtGui.QColor(int(array[i][j])))
+                self.gridWidgets[i][j].setColor(QtGui.QColor(int(array[i][j]), 100, 100)) # Assigns corresponding value to grid widget color
         
-    def parseSerial(self):
-        if(not self.input_ser.isOpen()):
-            return 
-        #print("Called")
-        count = 0
+    def parseSerial(self): # Parses the input from serial port and converts to array
+        if(not self.input_ser.isOpen()): # Skip and return zeros if there is nothing plugged in
+            return [[0]*4]*4
+        count = 0 # Indicates row
         temp_array = np.zeros((4,4))
+        started = False
         while self.input_ser.in_waiting:
             inStr = self.input_ser.readline()
-            #print(inStr)
             inputs = inStr.decode('utf-8').split(" ")
             line_temp = [string for string in inputs if string != ""]
-            if(len(line_temp) > 1):
-                line_int = [int(line_temp[i]) for i in range(len(line_temp)-1)]
+            if(line_temp[0] == 'S'):
+                started = True
+                print("Started")
 
+            if(len(line_temp) > 1 and started):
+                line_int = [int(line_temp[i]) for i in range(len(line_temp)-1)]
+                print(line_int)
                 if(np.shape(line_int)[0] == 4):
                     temp_array[count,:] = line_int
                     count += 1
 
                 if(count > 3):
                     #print(temp_array)
-                    break
-        return temp_array
+                    return temp_array
 
-
-       
 
 if (__name__ == "__main__"):
     app = QtWidgets.QApplication(sys.argv)
