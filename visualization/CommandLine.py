86c0bd8ff38092325dda8011155191ad014c10a2
Josh Oldenburg <josh@joldenburg.me>
2020-11-13 19:42:35 -0500

    Bump baud

--- visualization/CommandLine.py
+++ visualization/CommandLine.py
@@ -6,7 +6,7 @@ import time
 if __name__ == '__main__':
     spy = False
     with serial.serial_for_url('spy:///dev/cu.usbmodem1422' if spy else '/dev/cu.usbmodem1422') as ser:
-        ser.baudrate = 9600
+        ser.baudrate = 230400
         processor = SerialProcessor(ser)
         time.sleep(2)
         ser.write(b'\x01') # Get all known node addresses
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             elif msg == 0:
                 addr, data = result
                 print(f'Data from {addr:3d}:')
-                if True:
+                if False:
                     for arr in data:
                         print('             ', end='')
                         for point in arr:
